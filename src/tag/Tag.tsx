08798f85fda78876334f56c9805ca91174bb96df
白星 <baixingxing@onething.net>
2022-10-26 10:43:41 +0800

    feat: 添加tag组件

--- src/tag/Tag.tsx
+++ src/tag/Tag.tsx
@@ -1,26 +1,63 @@
-import React, { ReactNode, CSSProperties } from 'react';
+import React, { ReactNode, CSSProperties, useState, useEffect } from 'react';
 import classNames from 'classnames';
-
+import { CloseOutlined } from '@ant-design/icons';
 import './index.scss';
 
 export interface tagProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
-    type?: 'normal' | 'primary' | 'dashed' | 'link' | 'text';
-    size?: 'small' | 'medium' | 'large';
-    children?: ReactNode;
-    style?: CSSProperties;
+    closable?: boolean;
+    color?: string;
+    visible?: boolean;
+    onClose?: Function;
 }
 
 const Tag = (props: tagProps) => {
-    const {className, type = 'normal', size = 'medium', children, style, ...others} = props;
+    const {
+        className,
+        children,
+        closable,
+        color,
+        style: pstyle,
+        ...others
+    } = props;
+    const [visible, setVisible] = useState(true)
+    useEffect(()=>{
+        if ('visible' in props && typeof props.visible !== 'undefined') {
+            setVisible(props.visible)
+        }
+    }, [props.visible])
+    const customColor = color && color.match(/^#/);
 
     const cls = classNames({
-        'ant-btn': true,
-        [`ant-btn-${size}`]: size,
-        [`ant-btn-${type}`]: type,
+        'ant-tag': true,
+        ['ant-tag-'+color]: color && !customColor, 
         [className as string]: !!className
     })
-    return <span {...others} className={cls} style={style} >{children}</span>;
+    const style: CSSProperties = {...pstyle}
+    if (customColor) {
+        style.backgroundColor = color;
+    }
+
+    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        if (typeof props.onClose === 'function') {
+            props.onClose(e)
+        }
+        if (e.defaultPrevented) {
+            return
+        }
+        if (!('visible' in props)) {
+            setVisible(false);
+        }
+    }
+    if (!visible) {
+        return null
+    }
+    return <span {...others} className={cls} style={style} >
+        {children}
+        {closable ? 
+        // @ts-ignore
+        <CloseOutlined onClick={handleClick} /> : null}
+        </span>;
 }
 
 export default Tag;
